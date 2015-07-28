0d1bdcac3be38b1aeacf604c88b755d395c7cb9c
sclark01 <sclark.tieche@gmail.com>
2015-07-28 15:25:44 -0700

    reverted ex. 8 back so that the randomNumberGame does not inheirit the randomNumberGame from ex. 7 but is a standalone class

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/RandomNumberGameWithUserThatStores.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/RandomNumberGameWithUserThatStores.java
@@ -1,27 +1,48 @@
 package com.thoughtworks.tw101.exercises.exercise8;
 
-import com.thoughtworks.tw101.exercises.exercise7.RandomNumberGame;
+
+import com.thoughtworks.tw101.exercises.exercise7.RandomNumberGenerator;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class RandomNumberGameWithUserThatStores extends RandomNumberGame{
-    public RandomNumberGameWithUserThatStores(){
-        super(new UserThatStoresGuesses(new BufferedReader((new InputStreamReader(System.in)))));
-    }
-    @Override
-    public void playGame(){
-        try {
-            super.playGame();
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+public class RandomNumberGameWithUserThatStores{
+
+        private UserThatStoresGuesses user;
+        private final int NUMBER_TO_GUESS;
+
+        public RandomNumberGameWithUserThatStores(){
+            user = new UserThatStoresGuesses(new BufferedReader(new InputStreamReader(System.in)));
+            NUMBER_TO_GUESS = RandomNumberGenerator.randomNumberBetweenTwoValues(1, 100);
         }
-        printGuesses();
 
-    }
+        public void playGame() {
+            System.out.println("Welcome to the Random Number Game! Let's play!");
+            while(true) {
+                user.makeGuess();
+                if (checkForWin()) {
+                    System.out.println("Congrats! You got the number I was thinking of.");
+                    break;
+                }
+                coachUser();
+            }
+            printGuesses();
+        }
 
-    private void printGuesses() {
-        System.out.println("Before you got the answer, you guessed:");
-        ((UserThatStoresGuesses)user).printGuesses();
-    }
+        private void printGuesses() {
+            System.out.println("Before you got the answer, you guessed:");
+            user.printGuesses();
+        }
+
+        private void coachUser() {
+            if(user.getGuess() > NUMBER_TO_GUESS){
+                System.out.println("Try guessing a little lower!");
+                return;
+            }
+            System.out.println("Try guessing a little higher!");
+        }
+
+        private boolean checkForWin() {
+            return user.getGuess() == NUMBER_TO_GUESS;
+        }
 }
